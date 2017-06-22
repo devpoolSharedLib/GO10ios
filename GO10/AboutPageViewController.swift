a3cecda0ab3b0b1c4686e6d1165488f8a840479c
Jirapas Chiradechwiroj <Plooer@Jirapas-MacBook-Pro.local>
2017-06-22 15:01:25 +0700

    change version and add about page

--- GO10/AboutPageViewController.swift
+++ GO10/AboutPageViewController.swift
@@ -2,14 +2,14 @@
 //  AboutPageViewController.swift
 //  GO10
 //
-//  Created by Jirapas Chiradechwiroj on 6/16/2560 BE.
+//  Created by Jirapas Chiradechwiroj on 6/19/2560 BE.
 //  Copyright © 2560 Gosoft. All rights reserved.
 //
 
 import UIKit
 
 class AboutPageViewController: UIViewController {
-
+    
     @IBOutlet weak var appNameLbl: UILabel!
     @IBOutlet weak var versionNameLbl: UILabel!
     @IBOutlet weak var copyRightsTxtView: UITextView!
@@ -32,22 +32,22 @@ class AboutPageViewController: UIViewController {
             gotoTermBtn.titleLabel?.font = UIFont(name:"Helvetica Neue", size:17)
         }
     }
-
-
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
